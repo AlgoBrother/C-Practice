6e8906915d9b33e6326f1cd5362034cb0eaa7d9a
Ashwin Rajhans <ashwinreaper10@gmail.com>
2024-06-05 02:03:55 +0530

    Create variables.c

---
+++ variables.c
@@ -0,0 +1,15 @@
+#define PI 3.1425 // Pre determiner which tells us what variable is already pre defined
+#include <stdio.h> 
+int main(){
+  int a = 5;
+
+  float out;
+  out = 2.3;
+  const float constant = 9.8;
+  printf("%f and %f\n", constant, out);
+  printf("The \"value\" of pi is %f\n", PI);
+
+ 
+  
+  return 0;
+}
