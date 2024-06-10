a51c4bb5ecf773321d0eb2d426de470b7d497043
Ashwin Rajhans <ashwinreaper10@gmail.com>
2024-06-10 14:09:14 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -7,7 +7,8 @@ int main(){
   scanf("%d", &num);
   og_num = num;
   while(og_num != 0){
-    remainder = og_num%10;
+    remainder = og_num%10; // dividing the number by 10 and getting its remainder, if we take 153, the loop will first divide the the number by 10 leaving remainder 3, 
+    // then the loop will run again dividing 15 by 10 leaving 5, then again leaving the remainder 1. then the loop will break because og_num(Original Number) will now be equal to 0 making our while case 'False'.
     result += remainder * remainder * remainder;
     og_num /= 10; // removing the last digit from the number 
   }
